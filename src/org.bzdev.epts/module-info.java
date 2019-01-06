a9467089213e9b54ab3fad2a543fb172952f5ed7
Bill Zaumen <bzdev.org@gmail.com>
2019-01-05 20:07:33 -0800

    Add missing module-info.java file

---
+++ src/org.bzdev.epts/module-info.java
@@ -0,0 +1,12 @@
+open module org.bzdev.epts {
+    exports org.bzdev.epts;
+    requires java.base;
+    requires java.desktop;
+    requires java.scripting;
+    requires org.bzdev.base;
+    requires org.bzdev.desktop;
+    requires org.bzdev.obnaming;
+    requires org.bzdev.devqsim;
+    requires org.bzdev.anim2d;
+    requires org.bzdev.ejws;
+}
